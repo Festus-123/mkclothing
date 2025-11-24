14e97ba468cd64623878ada22f26a6322a0fea83
Festus Phillip <st.festus4cruise@gmail.com>
2025-11-24 19:09:44 +0100

    set up admin api to get dashboard and login

---
+++ frontend/src/api/adminApi.js
@@ -0,0 +1,32 @@
+const API_URL = "http://localhost:5000/api/admin";
+
+export const logIn = async (email, password) => {
+    try {
+        const res = await fetch(`${API_URL}/login`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ email, password }),
+        });
+        const data = await res.json();
+        if(!res.ok) throw new Error(data.message || "Failed to log in");
+        return data;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const getDashboard = async (token) => {
+    try {
+        const res = await fetch(`${API_URL}/dashboard`, {
+            method: "GET",
+            header: { authorization: `Bearer ${token}` },
+        });
+        const data = await res.json();
+        if(!res.ok) {
+            throw new Error(data.message || "Failed to fetch dashboard data");
+        }
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
