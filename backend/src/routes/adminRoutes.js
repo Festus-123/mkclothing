050cf01b9868166567f97f76ef39f597ac9ecdc4
Festus Phillip <st.festus4cruise@gmail.com>
2025-11-26 00:31:46 +0100

    dissabled sign up route

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -7,7 +7,10 @@ const router = express.Router();
 
 
 // admin sign up
-router.post('/signup', signUpAdmin);
+router.post('/signup', () => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(503).json({ message: 'Sign up is currently disabled' });
+});
 
 // admin log in
 router.post('/login', loginAdmin);
