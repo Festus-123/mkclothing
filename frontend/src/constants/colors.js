bff08f2decbf8e7d8a5ceba45a5b5d4ac16f85fb
Israel <kolybrytme@gmail.com>
2025-11-26 09:40:58 +0100

    Colors Update

---
+++ frontend/src/constants/colors.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const colors =  {
+  background: '#ffff',
+  container: '#ffff',
+  secondaryContainer: '#D9D9D9 #F8F3F3',
+  icon: '#AF1A1D',
+  button: '#FF383C #DD7325',
+  footerBackground: '#DD7325',
+  PTextColor: '#814518',
+  STextColor: '#ffff',
+  OtherTextColor: '#0000',
+  semiButton: '#CC6C24'
+}
+
+export default colors
