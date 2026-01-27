10e520dbd1fef1ab63e22efd9fb079a167bb1991
Festus Phillip <st.festus4cruise@gmail.com>
2026-01-28 00:19:07 +0100

    vite issue

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,11 +1,5 @@
 export default {
-  plugins: {
-<<<<<<< HEAD
-    "@tailwindcss/postcss": {},
-  }
+    plugins: {
+        '@tailwindcss/postcss' : {}
+    }
 }
-=======
-    '@tailwindcss/postcss': {},
-  },
-};
->>>>>>> d3609ea9797f88241f5515a98f0437a3baa3b0d7
