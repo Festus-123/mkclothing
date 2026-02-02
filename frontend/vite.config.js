e7044062d6c3ed3c1388a582b560aede5dea2966
Festus Phillip <st.festus4cruise@gmail.com>
2026-02-02 22:13:28 +0100

    fix crashing issue of supabase Url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
-
 import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  base: '/',   // 👈 MUST be this
 })
-
